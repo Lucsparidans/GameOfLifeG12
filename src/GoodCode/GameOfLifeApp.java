5b951d05d05e34b599d3d500bbe03adfd2aa4704
snadiraps <snadiraps@users.noreply.github.com>
2018-05-15 11:15:07 +0200

    Basic Window

--- src/GoodCode/GameOfLifeApp.java
+++ src/GoodCode/GameOfLifeApp.java
@@ -1,4 +1,27 @@
 package GoodCode;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Panel;
 
-public class GameOfLifeApp {
+public class GameOfLifeApp extends JFrame {
+    private int WIDTH = 600;
+    private int HEIGHT = 800;
+    private final JButton button = new JButton("pause");
+    private Panel panel = new Panel();
+    private GameOfLifeApp(){
+        super("Game of life");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(WIDTH,HEIGHT);
+        this.setLocationRelativeTo(null);
+        this.add(panel, BorderLayout.NORTH);
+        panel.add(button);
+        pack();
+        this.setVisible(true);
+    }
+    public static void main(String[] args){
+        GameOfLifeApp app = new GameOfLifeApp();
+
+    }
 }
