2b8a6dec42211575403b21e4e46a193228e4b4e4
StijnCoppens <styncoppens@gmail.com>
2018-05-15 11:04:56 +0200

    abstract

--- src/GoodCode/Cell.java
+++ src/GoodCode/Cell.java
@@ -1,4 +1,12 @@
 package GoodCode;
 
 public class Cell {
+    private CellState cellState;
+
+    public CellState getState(){
+        return cellState;
+    }
+    public void setState(CellState newState){
+        cellState = newState;
+    }
 }
